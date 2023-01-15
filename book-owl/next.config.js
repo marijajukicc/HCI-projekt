bdabad960277525e42c901c158cd52bdcdc3f09c
Adriana Gutesa <plavusa114@gmail.com>
2023-01-15 22:44:19 +0100

    .

--- book-owl/next.config.js
+++ book-owl/next.config.js
@@ -1,6 +1,14 @@
-module.exports = {
-  //reactStrictMode: true,
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  env: {
+    CONTENTFUL_SPACE_ID: process.env.CONTENTFUL_SPACE_ID,
+    CONTENTFUL_ACCESS_TOKEN: process.env.CONTENTFUL_ACCESS_TOKEN,
+    CONTENT_MANAGEMENT_API_KEY: process.env.CONTENT_MANAGEMENT_API_KEY,
+  },
   images: {
-    domains: ['images.ctfassets.net'],
+    domains: ["images.ctfassets.net"],
   },
 };
+
+module.exports = nextConfig;
