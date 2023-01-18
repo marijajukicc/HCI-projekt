8e55b41f70210544d61e608d42a6b135cba77aa7
adrianagutesa <gutesaa12@gmail.com>
2023-01-18 23:36:52 +0100

    Empty cart page

--- book-owl/pages/shoppingCart.js
+++ book-owl/pages/shoppingCart.js
@@ -1,13 +1,8 @@
-import HeaderFooterLayout from "../layouts/HeaderFooterLayout";
-import Background from "../layouts/background";
+import EmptyCart from "../components/shoppingCart/emptyCart";
 
 const ShoppingCart = () => {
     return (
-        <HeaderFooterLayout>
-            <Background>
-                <p className="uppercase text-3xl text-center text-shingle-fawn">Shopping Cart</p>
-            </Background>
-        </HeaderFooterLayout>
+        <EmptyCart />
     );
 };
 
