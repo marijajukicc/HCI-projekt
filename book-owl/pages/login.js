89dfd8d5261d835ee008e75ade97ad09561538a5
adrianagutesa <gutesaa12@gmail.com>
2023-02-14 22:34:04 +0100

    Responsive login

--- book-owl/pages/login.js
+++ book-owl/pages/login.js
@@ -39,9 +39,9 @@ const Login = () => {
             <title>BookOwl / Login</title>
             <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         </Head>
-        <main style={{background: `url(${bg.src})`,}} className="h-screen w-full">
-            <div className="flex flex-col justify-center items-center p-10">
-                <Link href="/" passHref className="w-[48vh]">
+        <main style={{background: `url(${bg.src})`,backgroundPositionX: 'center'}} className="relative h-screen w-full">
+            <div className="absolute top-1/2 left-1/2 m-0 -translate-x-1/2 -translate-y-1/2 flex flex-col justify-center items-center gap-8 lg:gap-10 p-5 sm:p-10">
+                <Link href="/" passHref className="md:w-[48vh] w-[42vh] mb-2">
                     <Image
                         src={BackArrow}
                         width={35}
@@ -50,34 +50,38 @@ const Login = () => {
                         className="hover:scale-110"
                     />
                 </Link>
-                <p className="text-shingle-fawn text-5xl text-center tracking-widest uppercase">Log in</p>
-                <section className="flex flex-col justify-center items-center">
-                    <div className="w-full relative mt-12">
-                        <label className="text-base text-shingle-fawn uppercase">Username</label>
-                        <input
-                            type="text"
-                            id="username"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                            className="inline-block w-full rounded-full text-l text-shingle-fawn bg-[#000000]/[.30] py-3 px-10 focus:outline-shingle-fawn"
-                        />
+                <p className="text-shingle-fawn text-4xl sm:text-5xl text-center tracking-widest uppercase">Log in</p>
+                <section className="flex flex-col justify-center items-center gap-8">
+                    <div className="flex flex-col gap-5">
+                        <div className="w-full relative ">
+                            <label className="text-base text-shingle-fawn uppercase">Username</label>
+                            <input
+                                type="text"
+                                id="username"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                                className="inline-block w-full rounded-full text-l text-shingle-fawn bg-[#000000]/[.30] py-3 px-10 focus:outline-shingle-fawn"
+                            />
+                        </div>
+                        <div className="w-full relative ">
+                            <label className="text-base text-shingle-fawn uppercase">Password</label>
+                            <input
+                                type="password"
+                                id="password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                className="inline-block w-full rounded-full text-l text-shingle-fawn bg-[#000000]/[.30] py-3 px-10 focus:outline-shingle-fawn"
+                            />
+                        </div>
+                        <button onClick={handleSubmit} className="self-center bg-light-brown/[.67] rounded-full px-7 py-4 uppercase text-base sm:text-xl hover:bg-light-brown hover:ring hover:ring-shingle-fawn hover:ring-offset-2 text-shingle-fawn-dark">Log in</button> 
+                        {error && <p className="mt-8 font-bold text-lg text-shingle-fawn">{error}</p>}
                     </div>
-                    <div className="w-full relative mt-8">
-                        <label className="text-base text-shingle-fawn uppercase">Password</label>
-                        <input
-                            type="password"
-                            id="password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                            className="inline-block w-full rounded-full text-l text-shingle-fawn bg-[#000000]/[.30] py-3 px-10 focus:outline-shingle-fawn"
-                        />
+                    <div>
+                        <p className="text-base sm:text-lg text-shingle-fawn">Don&#39;t have an account yet?</p>
+                        <Link href="/registration" key="registration" passHref>
+                            <button className="bg-light-brown/[.67] rounded-full px-7 py-4 uppercase text-base sm:text-xl hover:bg-light-brown hover:ring hover:ring-shingle-fawn hover:ring-offset-2 text-shingle-fawn-dark">Sign up now</button>     
+                        </Link>
                     </div>
-                    <button onClick={handleSubmit} className="bg-light-brown/[.67] rounded-full px-7 py-4 uppercase text-xl hover:bg-light-brown hover:ring hover:ring-shingle-fawn hover:ring-offset-2 text-shingle-fawn-dark mt-8">Log in</button> 
-                    {error && <p className="mt-8 font-bold text-lg text-shingle-fawn">{error}</p>}
-                    <p className="text-lg text-shingle-fawn mt-20">Don&#39;t have an account yet?</p>
-                    <Link href="/registration" key="registration" passHref>
-                        <button className="bg-light-brown/[.67] rounded-full px-7 py-4 uppercase text-xl hover:bg-light-brown hover:ring hover:ring-shingle-fawn hover:ring-offset-2 text-shingle-fawn-dark mt-4">Sign up now</button>     
-                    </Link>
                 </section>
             </div>
         </main> 
