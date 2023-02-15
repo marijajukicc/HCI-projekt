6cd17289d484e2cab6e053d96005d66f880c851d
adrianagutesa <gutesaa12@gmail.com>
2023-02-16 00:17:20 +0100

    Small fix

--- book-owl/layouts/HeaderFooterLayout.js
+++ book-owl/layouts/HeaderFooterLayout.js
@@ -2,10 +2,10 @@ import Header from "../components/header";
 import Footer from "../components/footer";
 import PageHead from "../components/pageHead";
 
-const HeaderFooterLayout = ({title, children, ...rest}) => {
+const HeaderFooterLayout = ({title, description, children, ...rest}) => {
     return(
         <div className="z-30">
-            <PageHead title={title} />
+            <PageHead title={title} description={description}/>
             <Header />
             <main {...rest}>{children}</main>
             <Footer />
